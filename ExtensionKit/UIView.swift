965bfa6202f28404e8f6ccbf05da9fea78efa41a
dengwenfeng <dwfn3398@corp.netease.com>
2016-05-24 10:47:08 +0800

    test

---
+++ ExtensionKit/UIView.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  ExtensionKit
+//
+//  Created by wen on 16/5/24.
+//  Copyright © 2016年 wenfeng. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func setRadius(radius: CGFloat) {
+        self.layer.cornerRadius = radius
+        self.layer.masksToBounds = true
+    }
+}
