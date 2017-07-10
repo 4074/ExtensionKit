b96b24b8dfd9f00c77f71cae3cd6c31196006fbb
4074 <137657976@qq.com>
2017-07-10 10:53:53 +0800

    modify TouchThroughView public

--- ExtensionKit/TouchThroughView.swift
+++ ExtensionKit/TouchThroughView.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2017年 wenfeng. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TouchThroughTableView: UITableView {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.next?.touchesBegan(touches, with: event)
+        super.touchesBegan(touches, with: event)
+    }
+}
+
+class TouchThroughCollectionView: UICollectionView {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.next?.touchesBegan(touches, with: event)
+        super.touchesBegan(touches, with: event)
+    }
+}
