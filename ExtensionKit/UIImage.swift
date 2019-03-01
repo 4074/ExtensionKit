866561dc594c6892839d4699c22c845b5093bf91
4074 <137657976@qq.com>
2019-03-01 15:50:21 +0800

    Image scale

--- ExtensionKit/UIImage.swift
+++ ExtensionKit/UIImage.swift
@@ -18,7 +18,7 @@ extension UIImage {
         
         let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
         
-        UIGraphicsBeginImageContextWithOptions(size, false, 1)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
         self.draw(in: rect)
         
         let image = UIGraphicsGetImageFromCurrentImageContext()!
